9b7cfa95455f68ea761dfda56a0094378d4363b0
nikunjpatel5136 <nikunjpatel5136@gmail.com>
2024-11-05 21:24:54 -0500

    add comments

--- totalCode.py
+++ totalCode.py
@@ -8,19 +8,22 @@ from sklearn.tree import DecisionTreeRegressor
 # Path of the file to read
 iowa_file_path = '../input/home-data-for-ml-course/train.csv'
 
+#set home_data as a readable table from the csv file
 home_data = pd.read_csv(iowa_file_path)
-# Create target object and call it y
+
+# Create prediction target object and call it y
 y = home_data.SalePrice
-# Create X
+
+# Create X by choosing features. These are the inputted colums that will be used to make predictions for the prediction object.
 features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 X = home_data[features]
 
-# Split into validation and training data
+# Split the prediction target and features into validation and training data
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
-# Specify Model
+# Specify Model using the Decision Tree Regressor
 iowa_model = DecisionTreeRegressor(random_state=1)
-# Fit Model
+# Fit Model using the training data
 iowa_model.fit(train_X, train_y)
 
 # Make validation predictions and calculate mean absolute error
@@ -30,10 +33,10 @@ print("Validation MAE: {:,.0f}".format(val_mae))
 
 from sklearn.ensemble import RandomForestRegressor
 
-# Define the model. Set random_state to 1
+# Define the model using the Random Forest Regressor. Set random_state to 1
 rf_model = RandomForestRegressor(random_state=1)
 
-# fit your model
+# Fit model using Training Data
 rf_model.fit(train_X,train_y)
 
 # Calculate the mean absolute error of your Random Forest model on the validation data
